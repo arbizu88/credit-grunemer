2dc4713e02d0bc0874349b94f8030114566d1fe1
Carlos Hidalgo <arbizu88@gmail.com>
2025-04-01 13:41:53 -0600

    add repositories

--- src/main/java/com/grunemer/crm/repository/ProfileRepository.java
+++ src/main/java/com/grunemer/crm/repository/ProfileRepository.java
@@ -1,6 +1,6 @@
 package com.grunemer.crm.repository;
 
-import com.grunemer.crm.dto.Profile;
+import com.grunemer.crm.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {}
